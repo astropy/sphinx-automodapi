0c9b6719e5f53b4d561c7bf6a54596392c740162
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-05-20 09:29:55 +0200

    Updated to the latest developer version of numpydoc

--- sphinx_automodapi/__init__.py
+++ sphinx_automodapi/__init__.py
@@ -1,9 +1,3 @@
-"""
-This package contains the sphinx extensions used by Astropy.
+from __future__ import division, absolute_import, print_function
 
-The `automodsumm` and `automodapi` modules contain extensions used by Astropy
-to generate API documentation - see the docstrings for details.  The `numpydoc`
-module is dervied from the documentation tools numpy and scipy use to parse
-docstrings in the numpy/scipy format, and are also used by Astropy.  The
-other modules are dependencies for `numpydoc`.
-"""
+from .numpydoc import setup
