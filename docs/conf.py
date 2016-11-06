7eb6cb597c642d2483253f1fd9734748e9365f29
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-11-06 16:12:11 +0000

    Add templates - package is now functional

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autosummary',
               'sphinx_automodapi.automodsumm',
               'sphinx_automodapi.automodapi']
 
@@ -156,7 +156,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
