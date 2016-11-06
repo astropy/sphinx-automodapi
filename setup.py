40793739ea69c963954bb6979bab534344110ca6
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-11-06 11:24:56 +0000

    Added initial setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from setuptools import setup
+
+setup(
+    name='sphinx-automodapi',
+    version='0.1.dev',
+    description='Sphinx extension for auto-generating API documentation for entire modules',
+    author='The Astropy Developers',
+    author_email='astropy.team@gmail.com',
+    license='BSD',
+    url='http://astropy.org',
+    zip_safe=False,
+    packages=['sphinx_automodapi'])
