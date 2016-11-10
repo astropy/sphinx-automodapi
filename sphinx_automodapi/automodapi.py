3e6af16ff79601a87b8501e620dc71f48058cbb0
Simon Conseil <contact@saimon.org>
2016-11-10 12:04:18 +0100

    Fix encoding error with automodapi_writereprocessed and py2

--- sphinx_automodapi/automodapi.py
+++ sphinx_automodapi/automodapi.py
@@ -83,6 +83,7 @@ This extension also adds two sphinx configuration options:
 # actually built.
 
 import inspect
+import io
 import os
 import re
 import sys
@@ -322,8 +323,9 @@ def automodapi_replace(sourcestr, app, dotoctree=True, docname=None,
                 ustr = newsourcestr.decode(app.config.source_encoding)
 
             if docname is None:
-                with open(os.path.join(app.srcdir, 'unknown.automodapi'), 'a') as f:
-                    f.write('\n**NEW DOC**\n\n')
+                with io.open(os.path.join(app.srcdir, 'unknown.automodapi'),
+                             'a', encoding='utf8') as f:
+                    f.write(u'\n**NEW DOC**\n\n')
                     f.write(ustr)
             else:
                 env = app.builder.env
@@ -332,7 +334,8 @@ def automodapi_replace(sourcestr, app, dotoctree=True, docname=None,
                 filename = docname + os.path.splitext(env.doc2path(docname))[1]
                 filename += '.automodapi'
 
-                with open(os.path.join(app.srcdir, filename), 'w') as f:
+                with io.open(os.path.join(app.srcdir, filename), 'w',
+                             encoding='utf8') as f:
                     f.write(ustr)
 
         return newsourcestr
