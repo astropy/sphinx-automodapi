b9ef1fee977849658722b323c6f9b6b8e6734ccc
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-12-10 10:12:17 +0000

    Added changelog, license, manifest file, and converted README file to rst

---
+++ CHANGES.rst
@@ -0,0 +1,13 @@
+Changes in sphinx-automodapi
+============================
+
+0.1 (unreleased)
+----------------
+
+- Fixed a bug that caused the automodapi directive to not work properly when
+  the main module docstring was not included. [#3]
+
+- Fixed a bug that caused skipped classes in automodapi directives to still
+  be included in inheritance diagrams. [#3]
+
+- Original code taken out of astropy-helpers
--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@
 
 from setuptools import setup
 
+with open('README.rst') as infile:
+    long_description = infile.read()
+
+from sphinx_automodapi import __version__
+
 setup(
     name='sphinx-automodapi',
-    version='0.1.dev',
+    version=__version__,
     description='Sphinx extension for auto-generating API documentation for entire modules',
+    long_description=long_description,
     author='The Astropy Developers',
     author_email='astropy.team@gmail.com',
     license='BSD',
