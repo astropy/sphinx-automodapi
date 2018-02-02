a0a62cbd5a927b6a455fce8e9e58491f87381a6c
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-02-02 14:39:44 +0000

    Switch to a better theme

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'sphinx-automodapidoc'
