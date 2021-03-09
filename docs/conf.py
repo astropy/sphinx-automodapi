6a5a176ec8a156c4663a99b5db4e022e1d57531b
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-03-09 14:58:31 -0500

    Revert out-of-scope changes

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The main toctree document.
-main_doc = 'index'
+master_doc = 'index'
 
 # General information about the project.
 project = 'sphinx-automodapi'
