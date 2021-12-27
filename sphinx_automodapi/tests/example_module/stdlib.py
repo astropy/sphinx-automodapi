baa95e98ac266eca63fe115ddb3a6ea08c493408
Ed Slavich <eslavich@stsci.edu>
2021-12-27 14:10:22 -0500

    Add test of :skip: with stdlib objects

---
+++ sphinx_automodapi/tests/example_module/stdlib.py
@@ -0,0 +1,12 @@
+"""
+A module that imports objects from the standard library.
+"""
+from pathlib import Path
+from datetime import time
+
+
+def add(a, b):
+    """
+    Add two numbers
+    """
+    return a + b
