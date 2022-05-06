39cd650c86b1c62c1ff0fe8c47070d38e889efe8
Simon Conseil <simon.conseil@lam.fr>
2022-05-06 22:21:22 +0200

    Add test

---
+++ sphinx_automodapi/tests/duplicated_warning/docs/conf.py
@@ -0,0 +1,11 @@
+project = 'duplicated'
+copyright = '2022, Maximilian Nöthe'
+author = 'Maximilian Nöthe'
+release = '0.1'
+
+
+extensions = [
+    "sphinx_automodapi.automodapi",
+]
+
+html_theme = 'alabaster'
