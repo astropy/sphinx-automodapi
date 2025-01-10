80641efd87da018dbbc4a77c984b8ea91c4aeb45
William Jamieson <william@jamieson.phd>
2025-01-10 14:38:21 -0500

    Add test case that mixes attributes with properties

---
+++ sphinx_automodapi/tests/cases/attribute_class/README.md
@@ -0,0 +1,2 @@
+This example is to make sure that classes can have attributes and properties
+and they can be distiguished if ``automodsumm_properties_are_attributes = False``
---
+++ sphinx_automodapi/tests/example_module/attribute_class.py
@@ -0,0 +1,12 @@
+class ClassWithAttribute(object):
+    """A class with an attribute."""
+    my_attribute = 1
+
+    def my_method(self):
+        """A method."""
+        pass
+
+    @property
+    def my_property(self):
+        """A property."""
+        return 1
